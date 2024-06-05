32c7590c174f5718a39156e6ebb616a883204218
tobiazab <tobiastzc@gmail.com>
2024-06-05 15:00:58 -0300

    Update scrip.js

--- shop/scrip.js
+++ shop/scrip.js
@@ -1,1 +1,39 @@
+function amebtn() {
+    hideAllCards();
+    document.querySelectorAll('.america').forEach(card => {
+        card.style.display = 'block';
+    });
+}
 
+function eurobtn() {
+    hideAllCards();
+    document.querySelectorAll('.europa').forEach(card => {
+        card.style.display = 'block';
+    });
+}
+
+function asiabtn() {
+    hideAllCards();
+    document.querySelectorAll('.asia').forEach(card => {
+        card.style.display = 'block';
+    });
+}
+
+function todosbtn() {
+    document.querySelectorAll('.card').forEach(card => {
+        card.style.display = 'block';
+    });
+}
+
+function oceabtn() {
+    hideAllCards();
+    document.querySelectorAll('.oceania').forEach(card => {
+        card.style.display = 'block';
+    });
+}
+
+function hideAllCards() {
+    document.querySelectorAll('.card').forEach(card => {
+        card.style.display = 'none';
+    });
+}
